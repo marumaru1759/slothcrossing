f2989b085d51b22823f448806589fa1545f1bfd2
marumaru1759 <marumaru1759@users.noreply.github.com>
2016-03-10 12:50:09 +0800

    car movement has been added

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ enchant ();
 window.onload = function () {
     var game = new Game(420,320);
     game.preload('asset/sloth.png');
-    game.preload('asset/hitsuji_nation.mp3');
+    game.preload('asset/hitsuji_nation.mp3','asset/car.jpg');
     game.fps = 30;
     
     game.onload = function(){
 
     // create start menu & play start music
-    game.assets['asset/hitsuji_nation.mp3'].play();
+    // game.assets['asset/hitsuji_nation.mp3'].play();
     var startscene = new Scene();
     
     startscene.backgroundColor = "#4169e1";
@@ -33,28 +33,39 @@ window.onload = function () {
     // display sloth(player) on the screen
     var firststage = new Scene();
     
-    var sprite = new Sprite(64, 51);
-    sprite.image = game.assets['asset/sloth.png'];
-    sprite.x = 20; sprite.y=150;
-    sprite.scaleX = -1;
-    firststage.addChild(sprite);
+    var sloth = new Sprite(64, 51);
+    sloth.image = game.assets['asset/sloth.png'];
+    sloth.x = 20; sloth.y=200;
+    sloth.scaleX = -1;
+    firststage.addChild(sloth);
+
+    var car = new Sprite(63,90);
+    var carloc = Math.round(Math.random() * 300) + 50;
+
+    car.image = game.assets['asset/car.jpg'];
+    car.x = carloc;  car.y= 10;
+    firststage.addChild(car);
 
         // to control sloth
-        game.addEventListener('enterframe',function(){
-            if(game.input.up){
-                sprite.y--;
-            }
-            if(game.input.down){
-                sprite.y++;
-            }
+        sloth.addEventListener('enterframe',function(){
             if(game.input.left){
-                sprite.x--;
-                sprite.scaleX = 1;
+                this.x--;
+                this.scaleX = 1;
            }
            if(game.input.right){
-                sprite.x++;
-                sprite.scaleX = -1;
+                this.x++;
+                this.scaleX = -1;
            }
+        })
+
+        // to move car
+        car.addEventListener('enterframe',function(){
+            this.y++;
+            if(this.y >= 200){
+                carloc = Math.round(Math.random() * 300) + 50;
+                this.x = carloc;
+                this.y = 10;
+            }
         })
 
     // transferred from title to first stage
